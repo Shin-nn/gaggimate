c5fa3ef6897405be8df9ddc8c8e0fc0849d82e81
Tomáš Černík <Tom.cernik@gmail.com>
2026-06-11 16:27:45 +0200

    playing with PID

--- lib/GaggiMateController/src/peripherals/Heater.h
+++ lib/GaggiMateController/src/peripherals/Heater.h
@@ -10,7 +10,7 @@
 enum class PIDLibrary { Legacy, Nimrod };
 
 constexpr float MAX_AUTOTUNE_TEMP = 125.0f;
-constexpr float TUNER_OUTPUT_SPAN = 1000.0f;
+constexpr float TUNER_OUTPUT_SPAN = 250.0f;
 
 using heater_error_callback_t = std::function<void()>;
 using heater_autotune_fail_callback_t = std::function<void()>;
